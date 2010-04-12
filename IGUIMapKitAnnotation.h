bfa41406ef1fdda5ff336b6c269275f9ba676406
ondrej.rafaj <ondrej.rafaj@59dd8bd1-9b00-f21c-293c-30cf7760cebe>
2010-04-12 13:43:07 +0000

    New mapkit section

---
+++ IGUIMapKitAnnotation.h
@@ -0,0 +1,16 @@
+//
+//  IGUIMapKitAnnotation.h
+//  IGUILibrary
+//
+//  Created by Ondrej Rafaj on 12.4.10.
+//  Copyright 2010 Home. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface IGUIMapKitAnnotation : NSObject {
+
+}
+
+@end
